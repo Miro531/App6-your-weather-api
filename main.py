9124a2d7cc80018ce25e058394748c7196887da3
Miro531 <popov.miroslav@hotmail.it>
2024-05-05 19:23:35 +0200

    Day 30: Return temperature for any station and date pt 2

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def home():
 def about(station, date):
     filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
-    temperature = df.loc[df['    DATE'] == date][]
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return { "station": station,
              "date": date,
              "temperature": temperature}
